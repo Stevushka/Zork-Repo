d3632dd74a67127223dd83d961a487a0291d6c34
Steven Fisher <stevushka.f@gmail.com>
2021-08-30 15:20:54 -0400

    Zork 2.1
    The Game World

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,13 +4,19 @@ namespace Zork
 {
     class Program
     {
+        private static string[] Rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
+        private static int currentRoom = 1;
+
         static void Main(string[] args)
         {
+            
+
             Console.WriteLine("Welcome to Zork!");
 
             Commands command = Commands.UNKNOWN;
             while(command != Commands.QUIT)
             {
+                Console.WriteLine(Rooms[currentRoom]);
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
@@ -24,9 +30,16 @@ namespace Zork
 
                     case Commands.NORTH: //If the command is any of the cardinal directions, tell the player which direction they chose.
                     case Commands.SOUTH:
+                        outputString = "The way is shut!";
+                        break;
+
                     case Commands.WEST:
                     case Commands.EAST:
-                        outputString = "You moved " + command.ToString() + ".";
+                        bool successfulMove = Move(command);
+                        if(successfulMove)
+                            outputString = $"You moved {command}.";
+                        else
+                            outputString = "The way is shut!";
                         break;
 
                     case Commands.QUIT: //If the command is quit, thank the player before the while loop catches it and exits.
@@ -41,6 +54,38 @@ namespace Zork
 
                 Console.WriteLine(outputString);
             }
+        }
+
+        private static bool Move(Commands direction)
+        {
+            bool successfulMove = true;
+
+            //Validate the direction
+            successfulMove = (direction == Commands.NORTH || direction == Commands.SOUTH
+                            || direction == Commands.EAST || direction == Commands.WEST) ? true : false;
+
+            //Process the validated direction
+            if(successfulMove)
+            {
+                switch(direction)
+                {
+                    case Commands.EAST:
+                        if(currentRoom < Rooms.Length - 1) //We haven't reached the right-most room
+                            currentRoom++;
+                        else
+                            successfulMove = false;
+                        break;
+
+                    case Commands.WEST:
+                        if(currentRoom > 0) //We haven't reached the first left-most room
+                            currentRoom--;
+                        else
+                            successfulMove = false;
+                        break;
+                }
+            }
+
+            return successfulMove; //return if the move was successful. True if nothing was wrong, false if something failed at any point
         }
 
         private static Commands ToCommand(string commandString)
