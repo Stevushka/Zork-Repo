dca9a19859d1bc596293b64ec0e36b2c54ef8164
Steven Fisher <stevushka.f@gmail.com>
2021-08-30 14:26:24 -0400

    Zork 1.3.1
    The Game Loop - Revision:
    Changed switch statement to use the outputString var instead of WriteLine each case.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -18,24 +18,24 @@ namespace Zork
                 switch(command)
                 {
                     case Commands.LOOK: //If the command is look, tell the player what they see.
-                        Console.WriteLine("This is an open field west of a white house, with a boarded front door." +
-                            "\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+                        outputString = "This is an open field west of a white house, with a boarded front door." +
+                            "\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
                         break;
 
                     case Commands.NORTH: //If the command is any of the cardinal directions, tell the player which direction they chose.
                     case Commands.SOUTH:
                     case Commands.WEST:
                     case Commands.EAST:
-                        Console.WriteLine("You moved " + command.ToString() + ".");
+                        outputString = "You moved " + command.ToString() + ".";
                         break;
 
                     case Commands.QUIT: //If the command is quit, thank the player before the while loop catches it and exits.
-                        Console.WriteLine("Thank you for playing!");
+                        outputString = "Thank you for playing!";
                         break;
 
                     case Commands.UNKNOWN: //If the command if UNKNOWN or somehow any othe string not in Commands, inform the player.
                     default:
-                        Console.WriteLine("Unknown Command.");
+                        outputString = "Unknown Command.";
                         break;
                 }
 
