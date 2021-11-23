1623f701e450f22e0726d97e8f228a136241d091
Steven Fisher <stevushka.f@gmail.com>
2021-11-22 23:54:13 -0500

    zork.unity pt2

--- Zork/Zork.Unity/Assets/Scripts/GameManager.cs
+++ Zork/Zork.Unity/Assets/Scripts/GameManager.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using Zork;
 using Newtonsoft.Json;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI CurrentLocationText;
+
+    [SerializeField]
+    private UnityInputService InputService;
+
+    [SerializeField]
+    private UnityOutputService OutputService;
+
     // Start is called before the first frame update
     void Start()
     {
         TextAsset gameTextAsset = Resources.Load<TextAsset>("Zork");
         Game game = JsonConvert.DeserializeObject<Game>(gameTextAsset.text);
+        
+        game.Start(InputService, OutputService);
+        CurrentLocationText.text = game.Player.Location.ToString();
     }
 }
--- Zork/Zork.Unity/Assets/Scripts/UnityOutputService.cs
+++ Zork/Zork.Unity/Assets/Scripts/UnityOutputService.cs
@@ -1,9 +1,73 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System.Collections.Generic;
 using Zork;
+using TMPro;
 
 public class UnityOutputService : MonoBehaviour, IOutputService
 {
+    [SerializeField]
+    private int MaxEntries = 60;
+
+    [SerializeField]
+    private Transform OutputTextContainer;
+
+    [SerializeField]
+    private TextMeshProUGUI TextLinePrefab;
+
+    [SerializeField]
+    private Image NewLinePrefab;
+
+    public UnityOutputService() => mEntries = new List<GameObject>();
+
+    public void Clear() => mEntries.ForEach(entry => Destroy(entry));
+
+    public void Write(string value) => ParseAndWriteLine(value);
+
+    public void WriteLine(string value) => ParseAndWriteLine(value);
+
+    private void ParseAndWriteLine(string value)
+    {
+        string[] delimeters = { "\n" };
+
+        var lines = value.Split(delimeters, StringSplitOptions.None);
+        foreach(var line in lines)
+        {
+            if(mEntries.Count >= MaxEntries)
+            {
+                var entry = mEntries.First();
+                Destroy(entry);
+                mEntries.Remove(entry);
+            }
+
+            if(string.IsNullOrWhiteSpace(line))
+            {
+                WriteNewLine();
+            }
+            else
+            {
+                WriteTextLine(line);
+            }
+        }
+    }
+
+    private void WriteNewLine()
+    {
+        var newLine = GameObject.Instantiate(NewLinePrefab);
+        newLine.transform.SetParent(OutputTextContainer, false);
+        mEntries.Add(newLine.gameObject);
+    }
+
+    private void WriteTextLine(string value)
+    {
+        var textLine = GameObject.Instantiate(TextLinePrefab);
+        textLine.transform.SetParent(OutputTextContainer, false);
+        textLine.text = value;
+        mEntries.Add(textLine.gameObject);
+    }
+
     public void Write(object value)
     {
         
@@ -13,4 +77,6 @@ public class UnityOutputService : MonoBehaviour, IOutputService
     {
         
     }
+
+    private readonly List<GameObject> mEntries;
 }
